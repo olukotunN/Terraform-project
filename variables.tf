3cf245f0917d145b70961cc0f669b2c712bc2fdc
olukotunN <niyibroklyn@gmail.com>
2022-12-16 19:03:57 +0000

    second commit

---
+++ variables.tf
@@ -0,0 +1,22 @@
+variable avail_zone {}
+variable "secrete_key" {
+    description = "secrete key"
+}
+
+variable "access_key" {
+    description = "access key"
+}
+
+variable subnet_cidr_block {}
+variable my_ip {}
+variable vpc_cidr_block {}
+#variable public_key_location {}
+  
+
+
+variable "provider_region" {
+    description = "provider region"
+}
+
+variable env_prefix {}
+variable instance_type {}
